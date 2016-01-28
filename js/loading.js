16d2511984ec9a260a9fdfad79918d86b7fef34f
lukewoolfson <lukw.woolfson@digital.justice.gov.uk>
2016-01-28 09:45:06 +0000

    1

--- js/loading.js
+++ js/loading.js
@@ -4,7 +4,7 @@ $(window).load(function(){
     
 /* buttons */
     
-$('input[value="Confirm"], input[value="Complete Application"], input[value="Next"], input[value="Search"], input[value="Select"], input[value="Submit Client"], input[value="Fast-Forward"]').on("click", function() {
+$('input[value="Confirm"], input[value="Complete Application"], input[value="Next"], input[value="Search"], input[value="Select"], input[value="Submit Client"], input[value="Fast-Forward"]' ).on("click", function() {
         var n = $(this),
             v = n.val(); 
      n.val("Loading").addClass("disabled");
@@ -17,4 +17,16 @@ $('.btnSubmitAmendment a, #id_CCMS_GN04_include_page_gn04_include_page_gn04_head
             t = n.text(); 
             n.text("Loading").addClass("disabled");
     });
+
+/* uploading */
+    
+  $('input[value="Upload"]' ).on("click", function() {
+        var n = $(this),
+            v = n.val(); 
+     n.val("Uploading").addClass("disabled");
+    });  
+    
 });
+
+
+
