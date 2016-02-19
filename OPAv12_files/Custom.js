2650546d9fe781659ec466e86cab07ec4d653d58
lukewoolfson <lukw.woolfson@digital.justice.gov.uk>
2016-02-19 09:45:56 +0000

    1

--- OPAv12_files/Custom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-   $('label:contains("HideMe")').parent().parent().hide();
-     $( "#scrollouthelp" ).hide();
-});
-function ScrollOut() {
-     $( "#scrollouthelp" ).slideToggle("slow");};
--- ccms/js/banner.js
+++
@@ -1,9 +0,0 @@
-(function (url) {
-  var div = document.createElement('div');
-  div.style.padding = '10px';
-  div.style.width = '1020px';
-  div.style.marginRight = 'auto';
-  div.style.marginLeft = 'auto';
-  div.innerHTML = '<p style="margin-top: 0px; margin-bottom: 0px;"><span>Please provide <a href="' + url + '" target="_blank">feedback</a> to help us improve this service.</span></p>';
-  document.body.insertBefore(div, document.body.firstChild);
-}('https://docs.google.com/forms/d/1qrJyYkxCl9dpSX_e2Usq0NvL5OxqfG8yMREmyxI2Mkw/viewform'));
